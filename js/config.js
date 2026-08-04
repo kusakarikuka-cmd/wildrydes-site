6baaf6ffca9f9a3c0e342307be2be66053290c02
kusakarikuka-cmd <kusakarikuka@gmail.com>
2026-08-04 10:45:11 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-1_FU0YMLb1Q', 
+        userPoolClientId: '4ounhvllh45jglssi4j5icqgfm', 
+        region: 'ap-northeast-1' 
+    },
+    api: {
+        invokeUrl: '' 
+    }
+};
